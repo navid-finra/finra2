481d49feb0e995621093d46ae8c3623d4b12493e
navid-finra <107125292+navid-finra@users.noreply.github.com>
2022-12-14 15:16:59 -0600

    Add files via upload

---
+++ imp1/Analysis.py
@@ -0,0 +1,187 @@
+#----------------------------------------------------------------------------------------------------------------#
+from Decision_Tree import Decision_Tree_Class
+#----------------------------------------------------------------------------------------------------------------#
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from tabulate import tabulate
+#----------------------------------------------------------------------------------------------------------------#
+from imblearn.over_sampling import SMOTE 
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier , DecisionTreeRegressor
+from sklearn.inspection import PartialDependenceDisplay 
+from sklearn.inspection import permutation_importance
+from PyALE import ale
+from lime.lime_tabular import LimeTabularExplainer
+from xgboost.sklearn import XGBRegressor
+import shap
+from sklearn.metrics import mean_squared_error
+import shap
+#----------------------------------------------------------------------------------------------------------------#
+
+class Analysis(Decision_Tree_Class):
+    def __init__(self,x_train,x_test,y_train,y_test,model):
+        
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
+        self.model = model
+
+        self.Error_analysis(self.x_train, self.y_train)
+        model_pred = self.model_pred
+        self.model_pred = model_pred
+        self.Error_analysis(self.x_test, self.y_test)
+        model_pred_test = self.model_pred
+        self.model_pred_test = model_pred_test    
+
+#----------------------------------------------------------------------------------------------------------------#
+
+    def Decision_model(self):
+
+        train_df =  pd.concat([self.x_train.reset_index(drop =True), self.y_train.reset_index(drop =True)], axis=1)
+        model_df_train = pd.DataFrame(self.model_pred, columns = ['pred_label']).reset_index(drop =True)
+        train_df = pd.concat([train_df, model_df_train], axis=1)
+        test_df =  pd.concat([self.x_test.reset_index(drop =True), self.y_test.reset_index(drop =True)], axis=1)
+        model_df_test = pd.DataFrame(self.model_pred_test, columns = ['pred_label']).reset_index(drop =True)
+        test_df = pd.concat([test_df, model_df_test], axis=1)
+        new_df = pd.concat([train_df, test_df], axis=0).reset_index(drop =True)
+        new_df['fn_error'] = None
+        for r in range(len(new_df)) :
+            if ((new_df[train_df.columns[-2]][r] == 1) and (new_df.pred_label[r] == 0)) :
+                new_df.fn_error[r] = 1
+            else :
+                new_df.fn_error[r] = 0
+        dt_df = new_df.drop([train_df.columns[-2], 'pred_label'], axis=1)
+
+        x_new = dt_df.drop(['fn_error'], axis=1)
+        y_new = dt_df.fn_error.astype('int')
+        sm = SMOTE(random_state=42)
+        x_blnc, y_blnc = sm.fit_resample(x_new, y_new)
+        self.x_train_dt, self.x_test_dt, self.y_train_dt, self.y_test_dt = train_test_split(x_blnc, y_blnc, train_size = 0.7, random_state = 42)
+
+        self.dt_clf = DecisionTreeClassifier(max_depth = 6, random_state = 0)
+        self.dt_clf.fit(self.x_train_dt, self.y_train_dt)
+
+#----------------------------------------------------------------------------------------------------------------#
+
+    def PDP(self):
+        self.Decision_model()
+        importances = pd.DataFrame({'feature':self.x_train_dt.columns,'importance':np.round(self.dt_clf.feature_importances_,3)})
+        importances = importances.sort_values('importance',ascending=False)
+        features = importances.feature[:2].tolist()
+
+        shap.plots.partial_dependence(
+            features[1], self.dt_clf.predict, X = self.x_train_dt, ice=False, pd_linewidth = 3, show=False
+        )
+
+        plt.savefig(f"./result/PDP_plot_{features[0]}.jpg")
+
+        shap.plots.partial_dependence(
+            features[0], self.dt_clf.predict, X = self.x_train_dt, ice=False, pd_linewidth = 3, show=False
+        )
+
+        plt.savefig(f"./result/PDP_plot_{features[1]}.jpg")
+    
+#----------------------------------------------------------------------------------------------------------------#
+
+    def ICE(self):
+        self.Decision_model()
+        importances = pd.DataFrame({'feature':self.x_train_dt.columns,'importance':np.round(self.dt_clf.feature_importances_,3)})
+        importances = importances.sort_values('importance',ascending=False)
+        features = importances.feature[:2].tolist()
+        PartialDependenceDisplay.from_estimator(self.dt_clf, self.x_train_dt, features ,kind='individual')
+        shap.plots.partial_dependence(
+            features[0], self.dt_clf.predict, self.x_train_dt, ice=True, ace_opacity=0.8, pd_linewidth = 3, show = False
+        )
+        plt.savefig(f"./result/ICE_plot_{features[1]}.jpg")
+
+        shap.plots.partial_dependence(
+            features[0], self.dt_clf.predict, self.x_train_dt, ice=True, ace_opacity=0.8, pd_linewidth = 3, show = False
+        )
+        plt.savefig(f"./result/ICE_plot_{features[1]}.jpg")
+
+#----------------------------------------------------------------------------------------------------------------#    
+
+    def ALE(self):
+        self.Decision_model()
+        importances = pd.DataFrame({'feature':self.x_train_dt.columns,'importance':np.round(self.dt_clf.feature_importances_,3)})
+        importances = importances.sort_values('importance',ascending=False)
+        features = importances.feature[:2].tolist()
+        ale_eff = ale(X=self.x_train_dt, model=self.dt_clf, feature=[features[0]], grid_size=50, include_CI=True, C=0.95)
+        plt.savefig("./result/ALE_1D_plot.jpg")
+        ale_eff2D = ale(X=self.x_train_dt, model=self.dt_clf, feature=features, grid_size=100)
+        plt.savefig("./result/ALE_2D_plot.jpg")
+
+#----------------------------------------------------------------------------------------------------------------#
+
+    def Permutation_feature_importance(self):
+        self.Decision_model()
+        r = permutation_importance(self.dt_clf, self.x_train_dt, self.y_train_dt,
+                           n_repeats=30,
+                           random_state=0)
+        a = []
+        column_name = []
+        column_value = []
+        for i in r.importances_mean.argsort()[::-1]:
+            if r.importances_mean[i] - 2 * r.importances_std[i] > 0:
+                a.append(i)
+                column_name.append(f"{self.x_train.columns[i]:<8}")
+                column_value.append(f"{r.importances_mean[i]:.3f} +/- {r.importances_std[i]:.3f}")
+        Permutation_feature_importance_df = pd.DataFrame({'feature':column_name , 'value':column_value})
+        Permutation_feature_importance_df.to_csv('./result/Permutation_feature_importance.csv') 
+        print('-------- Permutation feature importance --------')
+        print(tabulate(Permutation_feature_importance_df, headers = 'keys', tablefmt = 'psql'), '\n')
+        
+#----------------------------------------------------------------------------------------------------------------#
+
+    def Global_Surrogate(self):
+        self.Decision_model()
+        new_target = self.dt_clf.predict(self.x_train_dt)
+
+        # defining the interpretable decision tree model
+        dt_model1 = DecisionTreeRegressor(max_depth=5, random_state=10)
+
+        # fitting the surrogate decision tree model using the training set and new target
+        dt_model1.fit(self.x_train_dt,new_target)
+
+#----------------------------------------------------------------------------------------------------------------#
+
+    def LIME(self):
+        self.Decision_model()
+        explainer = LimeTabularExplainer(self.x_train_dt.values, mode="regression", feature_names=self.x_train_dt.columns)
+        r = permutation_importance(self.dt_clf, self.x_train_dt, self.y_train_dt,
+                           n_repeats=30,
+                           random_state=0)
+        a = []
+        for i in r.importances_mean.argsort()[::-1]:
+            if r.importances_mean[i] - 2 * r.importances_std[i] > 0:
+                a.append(i)
+        # storing a new observation
+        i = 6
+        feature_name = []
+        feature_value = []
+        for i in a:
+            X_observation = self.x_test_dt.iloc[[i], :]
+            feature_name.append(self.x_train.columns[i])
+            feature_value.append(self.dt_clf.predict(X_observation)[0])
+           
+        RF_prediction_df = pd.DataFrame({'feature':feature_name , 'value':feature_value})
+        RF_prediction_df.to_csv('./result/RF_prediction.csv')
+
+        explanation = explainer.explain_instance(X_observation.values[0], self.dt_clf.predict)
+        explanation.show_in_notebook(show_table=True, show_all=False)
+
+#----------------------------------------------------------------------------------------------------------------#
+
+    def shaply(self):
+        self.Decision_model()
+        xgb_model = XGBRegressor(n_estimators=1000, max_depth=10, learning_rate=0.001, random_state=0)
+        xgb_model.fit(self.x_train_dt, self.y_train_dt)
+        y_predict = xgb_model.predict(self.x_test_dt)
+        mean_squared_error(self.y_test_dt, y_predict)**(0.5)
+        explainer = shap.TreeExplainer(xgb_model)
+        shap_values = explainer.shap_values(self.x_train_dt)
+        shap.summary_plot(shap_values, features = self.x_train_dt, feature_names = self.x_train_dt.columns)
+
+#----------------------------------------------------------------------------------------------------------------#
---
+++ imp1/Edge_case.py
@@ -0,0 +1,68 @@
+#----------------------------------------------------------------------------------------------------------------#
+import logging
+import pandas as pd
+#----------------------------------------------------------------------------------------------------------------#
+
+class  Edge_case:
+    def __init__(self,x_train,x_test,y_train,y_test,model):
+        
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
+        self.model = model 
+
+    def edge_case_analysis(self):
+        model = self.model
+        x_train = self.x_train
+        x_test = self.x_test
+
+        binary_feats = [col for col in x_train if 
+                    x_train[col].dropna().value_counts().index.isin([0,1]).all()]
+
+        num_feats = x_train.drop(columns = binary_feats).columns
+        #  epsilon for edge case
+        eps = 0.1
+        edge_df = pd.DataFrame()
+        for c in range(len(num_feats)):    
+            pred_label = model.predict(x_test).tolist()
+
+            # value + epsilon
+            x_up = x_test.copy()
+            x_up.loc[:, num_feats[c]] = x_up.loc[:, num_feats[c]] +  x_up.loc[:, num_feats[c]] * eps
+            pred_up = model.predict(x_up).tolist()
+
+            # value - epsilon
+            x_dwn = x_test.copy()
+            x_dwn.loc[:, num_feats[c]] = x_dwn.loc[:, num_feats[c]] -  x_dwn.loc[:, num_feats[c]] * eps
+            pred_dwn = model.predict(x_dwn).tolist()
+
+            df_ = pd.DataFrame({ 'pred_label' : pred_label, 'pred_label_up' : pred_up, 'pred_label_down' : pred_dwn })
+            df_['features'] = num_feats[c]
+            edge_df = pd.concat([edge_df, df_], axis = 0)
+
+        edge_df['edge_case_up'] = edge_df['pred_label'] != edge_df['pred_label_up']
+        edge_df['edge_case_down'] = edge_df['pred_label'] != edge_df['pred_label_down']
+        print(f'all_edge_cases = {round((sum(edge_df.edge_case_up) + sum(edge_df.edge_case_down)) / (x_test.shape[0] * x_test.shape[1]),3) } %')
+        edge_df_pos = edge_df[edge_df.pred_label == 1]
+        print(f'pos_edge_cases = {round((sum(edge_df_pos.edge_case_up) + sum(edge_df_pos.edge_case_down)) / ((edge_df_pos.shape[0] * edge_df_pos.shape[1])+0.1),3) } %')
+        edge_df_neg = edge_df[edge_df.pred_label == 0]
+        print(f'neg_edge_cases = {round((sum(edge_df_neg.edge_case_up) + sum(edge_df_neg.edge_case_down)) / ((edge_df_neg.shape[0] * edge_df_neg.shape[1])+0.01),3) } %\n\n\n')
+
+        up_edge_cases_features = pd.DataFrame(edge_df[edge_df.edge_case_up == True].groupby("features").size(), columns = ['#']).sort_values('#', ascending=False)
+        print(f'up_edge_cases_features : \n{up_edge_cases_features}\n\n\n')
+
+        down_edge_cases_features = pd.DataFrame(edge_df[edge_df.edge_case_down == True].groupby("features").size(), columns = ['#']).sort_values('#', ascending=False)
+        print(f'down_edge_cases_features : \n{down_edge_cases_features}')
+
+        edge_df.to_csv('./edge_case_df.csv')
+        up_edge_cases_features.to_csv('./up_edge_cases_features.csv')
+        down_edge_cases_features.to_csv('./down_edge_cases_features.csv')
+
+        logging.debug(f'all_edge_cases = {(sum(edge_df.edge_case_up) + sum(edge_df.edge_case_down)) / (x_test.shape[0] * x_test.shape[1]) } %')
+        logging.debug(f'pos_edge_cases = {(sum(edge_df_pos.edge_case_up) + sum(edge_df_pos.edge_case_down)) / (edge_df_pos.shape[0] * edge_df_pos.shape[1]) } %')
+        logging.debug(f'neg_edge_cases = {(sum(edge_df_neg.edge_case_up) + sum(edge_df_neg.edge_case_down)) / (edge_df_neg.shape[0] * edge_df_neg.shape[1]) } %\n\n\n')
+        logging.debug('-------------------------------------------------------------------------------')
+        del edge_df, edge_df_pos, edge_df_neg, up_edge_cases_features, down_edge_cases_features
+
+#----------------------------------------------------------------------------------------------------------------#
