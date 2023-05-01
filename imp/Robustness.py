ec5bc5e08b8206508c55f7f07d595d6ba4576c95
navid-finra <107125292+navid-finra@users.noreply.github.com>
2023-05-01 19:21:47 +0330

    Add files via upload

--- imp/Robustness.py
+++ imp/Robustness.py
@@ -17,11 +17,17 @@ class  Robustness:
         y_predict = model.predict(self.data.x_test).reshape(1,-1)[0]
         y_predict = np.array([int(round(x)) for x in y_predict])
         mean_x = self.data.x_test.mean()/100
+        zero_mean = pd.DataFrame((mean_x == 0), columns = ['bool'])
+        zero_mean = zero_mean.index[zero_mean['bool']==True].tolist()
 
         for j in range(1,101):
             for i in num_feats:
-                up[i] += mean_x[i] * j
-                down[i] -= mean_x[i] * j
+                if i in zero_mean:
+                    up[i] += j*(0.01)
+                    down[i] -= j*(0.01)
+                else:
+                    up[i] += mean_x[i] * j
+                    down[i] -= mean_x[i] * j
                 up_pre = self.data.model.predict(up).reshape(1,-1)[0]
                 up_pre = np.array([int(round(x)) for x in up_pre])
                 up_arr = (up_pre == y_predict) + 0
