46c030510fb8a756818b68468e35e0da3bae6910
navid-finra <107125292+navid-finra@users.noreply.github.com>
2023-05-01 15:58:37 +0330

    Add files via upload

--- imp/Edge_case.py
+++ imp/Edge_case.py
@@ -4,44 +4,59 @@ import pandas as pd
 
 class  Edge_case:
     def __init__(self, data):
+        
         self.data = data
 
     def edge_case_analysis(self):
-        try:
-            num_feats = self.data.x_test.columns
-        except:
-            num_feats = pd.DataFrame(self.data.x_test).columns
+        model = self.data.model
+        x_train = self.data.x_train
+        x_test = self.data.x_test
 
-        up = self.data.x_test.copy()
-        down = self.data.x_test.copy()
-        y_predict = model.predict(self.data.x_test).reshape(1,-1)[0]
-        y_predict = np.array([int(round(x)) for x in y_predict])
-        mean_x = self.data.x_test.mean()/100
+        binary_feats = [col for col in x_train if x_train[col].dropna().value_counts().index.isin([0,1]).all()]
 
-        for j in range(1,101):
-            for i in num_feats:
-                up[i] += mean_x[i] * j
-                down[i] -= mean_x[i] * j
-                up_pre = self.data.model.predict(up).reshape(1,-1)[0]
-                up_pre = np.array([int(round(x)) for x in up_pre])
-                up_arr = (up_pre == y_predict) + 0
+        num_feats = x_train.drop(columns = binary_feats).columns
+        #  epsilon for edge case
+        eps = 0.05
+        edge_df = pd.DataFrame()
+        for c in range(len(num_feats)):    
+            pred_label = model.predict(x_test).tolist()
 
-                down_pre = self.data.model.predict(down).reshape(1,-1)[0]
-                down_pre = np.array([int(round(x)) for x in down_pre])
-                down_arr = (down_pre == y_predict) + 0
+            # value + epsilon
+            x_up = x_test.copy()
+            x_up.loc[:, num_feats[c]] = x_up.loc[:, num_feats[c]] +  x_up.loc[:, num_feats[c]] * eps
+            pred_up = model.predict(x_up).tolist()
 
-                up['y_pred'] = y_predict
-                up['predict'] = down_arr + up_arr
-                up = up[up['predict'] == 2]
-                y_predict = up['y_pred']
-                up = up.drop(['y_pred','predict'], axis = 1)
+            # value - epsilon
+            x_dwn = x_test.copy()
+            x_dwn.loc[:, num_feats[c]] = x_dwn.loc[:, num_feats[c]] -  x_dwn.loc[:, num_feats[c]] * eps
+            pred_dwn = model.predict(x_dwn).tolist()
 
-                down['y_pred'] = y_predict
-                down['predict'] = down_arr + up_arr
-                down = down[down['predict'] == 2]
-                down = down.drop(['y_pred','predict'], axis = 1)
-            up -= mean_x * j
-            down += mean_x * j
+            df_ = pd.DataFrame({ 'pred_label' : pred_label, 'pred_label_up' : pred_up, 'pred_label_down' : pred_dwn })
+            df_['features'] = num_feats[c]
+            edge_df = pd.concat([edge_df, df_], axis = 0)
 
-        print('Robustness =', round(up.shape[0]/self.data.x_test.shape[0],2),'%')
+        edge_df['edge_case_up'] = edge_df['pred_label'] != edge_df['pred_label_up']
+        edge_df['edge_case_down'] = edge_df['pred_label'] != edge_df['pred_label_down']
+        print(f'all_edge_cases = {round((sum(edge_df.edge_case_up) + sum(edge_df.edge_case_down)) / (x_test.shape[0] * x_test.shape[1]),3) } %')
+        edge_df_pos = edge_df[edge_df.pred_label == 1]
+        print(f'pos_edge_cases = {round((sum(edge_df_pos.edge_case_up) + sum(edge_df_pos.edge_case_down)) / ((edge_df_pos.shape[0] * edge_df_pos.shape[1])+0.01),3) } %')
+        edge_df_neg = edge_df[edge_df.pred_label == 0]
+        print(f'neg_edge_cases = {round((sum(edge_df_neg.edge_case_up) + sum(edge_df_neg.edge_case_down)) / ((edge_df_neg.shape[0] * edge_df_neg.shape[1])+0.01),3) } %\n\n\n')
+
+        up_edge_cases_features = pd.DataFrame(edge_df[edge_df.edge_case_up == True].groupby("features").size(), columns = ['#']).sort_values('#', ascending=False)
+        print(f'up_edge_cases_features : \n{up_edge_cases_features}\n\n\n')
+
+        down_edge_cases_features = pd.DataFrame(edge_df[edge_df.edge_case_down == True].groupby("features").size(), columns = ['#']).sort_values('#', ascending=False)
+        print(f'down_edge_cases_features : \n{down_edge_cases_features}')
+
+        edge_df.to_csv('./edge_case_df.csv')
+        up_edge_cases_features.to_csv('./up_edge_cases_features.csv')
+        down_edge_cases_features.to_csv('./down_edge_cases_features.csv')
+
+        print(f'all_edge_cases = {(sum(edge_df.edge_case_up) + sum(edge_df.edge_case_down)) / ((x_test.shape[0] * x_test.shape[1])+0.01) } %')
+        print(f'pos_edge_cases = {(sum(edge_df_pos.edge_case_up) + sum(edge_df_pos.edge_case_down)) / ((edge_df_pos.shape[0] * edge_df_pos.shape[1])+0.01) } %')
+        print(f'neg_edge_cases = {(sum(edge_df_neg.edge_case_up) + sum(edge_df_neg.edge_case_down)) / ((edge_df_neg.shape[0] * edge_df_neg.shape[1])+0.01) } %\n\n\n')
+        return down_edge_cases_features, up_edge_cases_features
+        
+
 #----------------------------------------------------------------------------------------------------------------#
---
+++ imp/Robustness.py
@@ -0,0 +1,47 @@
+#----------------------------------------------------------------------------------------------------------------#
+import pandas as pd
+#----------------------------------------------------------------------------------------------------------------#
+
+class  Robustness:
+    def __init__(self, data):
+        self.data = data
+
+    def robustness_analysis(self):
+        try:
+            num_feats = self.data.x_test.columns
+        except:
+            num_feats = pd.DataFrame(self.data.x_test).columns
+
+        up = self.data.x_test.copy()
+        down = self.data.x_test.copy()
+        y_predict = model.predict(self.data.x_test).reshape(1,-1)[0]
+        y_predict = np.array([int(round(x)) for x in y_predict])
+        mean_x = self.data.x_test.mean()/100
+
+        for j in range(1,101):
+            for i in num_feats:
+                up[i] += mean_x[i] * j
+                down[i] -= mean_x[i] * j
+                up_pre = self.data.model.predict(up).reshape(1,-1)[0]
+                up_pre = np.array([int(round(x)) for x in up_pre])
+                up_arr = (up_pre == y_predict) + 0
+
+                down_pre = self.data.model.predict(down).reshape(1,-1)[0]
+                down_pre = np.array([int(round(x)) for x in down_pre])
+                down_arr = (down_pre == y_predict) + 0
+
+                up['y_pred'] = y_predict
+                up['predict'] = down_arr + up_arr
+                up = up[up['predict'] == 2]
+                y_predict = up['y_pred']
+                up = up.drop(['y_pred','predict'], axis = 1)
+
+                down['y_pred'] = y_predict
+                down['predict'] = down_arr + up_arr
+                down = down[down['predict'] == 2]
+                down = down.drop(['y_pred','predict'], axis = 1)
+            up -= mean_x * j
+            down += mean_x * j
+
+        print('Robustness =', round(up.shape[0]/self.data.x_test.shape[0],2),'%')
+#----------------------------------------------------------------------------------------------------------------#
