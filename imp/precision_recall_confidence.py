f7a94d5530e3d47c9f4161e92a24645a1dbccaa0
navid-finra <107125292+navid-finra@users.noreply.github.com>
2023-07-31 19:31:37 +0330

    Update precision_recall_confidence.py

--- imp/precision_recall_confidence.py
+++ imp/precision_recall_confidence.py
@@ -12,28 +12,37 @@ class Precision_Recall_Confidence:
         self.fn = conf_mat[1][0]
         self.tp = conf_mat[1][1]
         
+    def interval(self, limit, n):
         
-    def recall(self, limit):
-        n = self.tp + self.fn
+        #probability
         p_hat = self.tp / n
-        
         if p_hat == 1:
             prob = 100 
         else:
             std = ((p_hat * (1-p_hat)) / n) ** 0.5
             z_score = (limit - p_hat)/std
             prob = round(1 - st.norm.cdf(z_score),3)*100
-        print(f'With probability {prob}%, recall is greater than {limit}')
+            
+        #confidence interval
+        alpha = 1-limit
+        z_score = norm.isf(alpha * 0.5) 
+        variance_of_sum = p_hat * (1-p_hat) / n
+        std = variance_of_sum ** 0.5
+        upper_bound = round(p_hat - z_score * std, 2)
+        lower_bound = round(p_hat + z_score * std, 2)
+        
+        return prob, upper_bound, lower_bound 
+        
+        
+    def recall(self, limit):
+        n = self.tp + self.fn
+        prob, upper_bound, lower_bound = self.interval(limit, n)
+        print(f'With probability {prob}%, recall is greater than {limit}.\n')
+        print(f'The {int(limit*100)}% confidence interval for precision is between {upper_bound} and {lower_bound}.')
         
         
     def precision(self, limit):
         n = self.tp + self.fp
-        p_hat = self.tp / n
-        
-        if p_hat == 1:
-            prob = 100 
-        else:
-            std = ((p_hat * (1-p_hat)) / n) ** 0.5
-            z_score = (limit - p_hat)/std
-            prob = round(1 - st.norm.cdf(z_score),3)*100
-        print(f'With probability {prob}%, precision is greater than {limit}')
+        prob, upper_bound, lower_bound = self.interval(limit, n)
+        print(f'With probability {prob}%, precision is greater than {limit}.\n')
+        print(f'The {int(limit*100)}% confidence interval for precision is between {upper_bound} and {lower_bound}.')
