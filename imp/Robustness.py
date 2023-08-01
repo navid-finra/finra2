10735c69228dfbcdf62d2a487b1408364d3a4357
navid-finra <107125292+navid-finra@users.noreply.github.com>
2023-08-01 20:56:18 +0330

    Update Robustness.py

--- imp/Robustness.py
+++ imp/Robustness.py
@@ -33,34 +33,33 @@ class  Robustness:
                 if up.shape[0] == 0:
                     break
             
-                else:    
-                    up_pre = self.data.model.predict(up).reshape(1,-1)[0]
+                else:
+                    up_pre = model.predict(up).reshape(1,-1)[0]
                     up_pre = np.array([int(round(x)) for x in up_pre])
                     up_arr = (up_pre == y_predict) + 0
-
-                    down_pre = self.data.model.predict(down).reshape(1,-1)[0]
+                    down_pre = model.predict(down).reshape(1,-1)[0]
                     down_pre = np.array([int(round(x)) for x in down_pre])
                     down_arr = (down_pre == y_predict) + 0
-
                     up['y_pred'] = y_predict
+                    down['y_pred'] = y_predict
                     up['predict'] = down_arr + up_arr
+                    down['predict'] = down_arr + up_arr
                     up = up[up['predict'] == 2]
+                    down = down[down['predict'] == 2]
+                    
                     y_predict = up['y_pred']
                     up = up.drop(['y_pred','predict'], axis = 1)
-
-                    down['y_pred'] = y_predict
-                    down['predict'] = down_arr + up_arr
-                    down = down[down['predict'] == 2]
                     down = down.drop(['y_pred','predict'], axis = 1)
-                
+        
             else:
+        
                 continue
-
+            
             break
-
                 
             up -= mean_x * j
             down += mean_x * j
-
-        print('Robustness =', round(up.shape[0]/self.data.x_test.shape[0],2),'%')
+        
+            
+        print('Robustness =', round(up.shape[0]/X_test.shape[0],2),'%')
 #----------------------------------------------------------------------------------------------------------------#
